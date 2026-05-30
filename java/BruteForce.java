5344208dcb350e068165ebcf68b57f0e5fe84f85
yakstttuiy-tech <yakstttuiy@gmail.com>
2026-05-30 05:17:41 +0300

    Добавлен класс BruteForce с методом decryptByBruteForce

--- java/BruteForce.java
+++ java/BruteForce.java
@@ -1,2 +1,14 @@
 public class BruteForce {
+    private static final char[] ALPHABET = Cipher.getAlphabet();
+    private final Cipher cipher = new Cipher();
+
+    public String decryptByBruteForce(String encryptedText) {
+        for (int key = 1; key < ALPHABET.length; key++) {
+            String decrypted = cipher.decrypt(encryptedText, key);
+            if (decrypted.contains(" ") && decrypted.toLowerCase().contains("что")) {
+                return decrypted;
+            }
+        }
+        return cipher.decrypt(encryptedText, 1);
+    }
 }
