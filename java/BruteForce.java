abc16bc40601c9b597a25701af9868228163dfa5
yakstttuiy-tech <yakstttuiy@gmail.com>
2026-05-30 05:25:20 +0300

    Добавлен словарь русских слов и метод looksLikeRussian для эвристической проверки успешности расшифровки

--- java/BruteForce.java
+++ java/BruteForce.java
@@ -11,4 +11,19 @@ public class BruteForce {
         }
         return cipher.decrypt(encryptedText, 1);
     }
+    private static final String[] RUSSIAN_WORDS = {
+            "что", "это", "и", "в", "на", "с", "по", "к", "у", "за",
+            "из", "о", "от", "для", "как", "так", "все", "было", "его",
+            "она", "они", "мы", "вы", "ты"
+    };
+
+    private boolean looksLikeRussian(String text) {
+        String lowerText = text.toLowerCase();
+        for (String word : RUSSIAN_WORDS) {
+            if (lowerText.contains(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
